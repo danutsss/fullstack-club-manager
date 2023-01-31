2d1be0b97cb1052512f190524067c81d44d6c544
Matei Nicolae - Daniel <matei1337@icloud.com>
2023-01-31 15:32:21 +0200

    modified header component in home page and `add-coach` page

--- pages/add-coach.vue
+++ pages/add-coach.vue
@@ -1,7 +1,23 @@
 <template>
 	<LayoutNavBar />
-	<div class="container h-screen font-display">
-		<p>hello from /add-coach</p>
-	</div>
+
+	<LayoutHeader>
+		<template v-slot:main-text>Antrenori Judo</template>
+		<template v-slot:secondary-text
+			>Contribuie la crearea unei liste cu toti antrenorii de Judo din
+			Romania.</template
+		>
+		<template v-slot:header-img>
+			<img
+				class="sm:w-3/4 sm:h-3/4 mt-2 shadow-2xl aspect-square rotate-3 rounded-2xl bg-zinc-100 dark:bg-zinc-800"
+				src="~/assets/images/judo-time.jpg"
+				alt="Judo"
+				loading="lazy"
+				style="color: transparent"
+				decoding="async"
+			/>
+		</template>
+	</LayoutHeader>
+
 	<LayoutFooter />
 </template>
