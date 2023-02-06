640856439718622ccffbf26cb8ad53c756679902
Matei Nicolae - Daniel <matei1337@icloud.com>
2023-02-06 18:03:00 +0200

    sign up functionality, user's role is saved into supabase db

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -96,4 +96,28 @@
 <script setup>
 import { Disclosure, DisclosurePanel } from "@headlessui/vue";
 import { LockClosedIcon } from "@heroicons/vue/20/solid";
+import { createClient } from "@supabase/supabase-js";
+
+const emailAddress = ref("");
+const userPassword = ref("");
+
+const supabaseUrl = "https://nkhpdpsosekugclzsydr.supabase.co";
+const supabaseKey =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5raHBkcHNvc2VrdWdjbHpzeWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU2OTQzNTQsImV4cCI6MTk5MTI3MDM1NH0.Uu5DE59P1tvHmCXtTVca7UrOYQKMDEuEI5HLjHHLPIQ";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const signIn = async () => {
+	let { data, error } = await supabase.auth.signInWithPassword({
+		email: emailAddress.value,
+		password: userPassword.value,
+	});
+
+	// const { data: profile } = await supabase.from("profiles").select("role");
+	// console.log(profile);
+
+	if (data) console.log(data);
+	if (error) console.log(error);
+
+	return;
+};
 </script>
