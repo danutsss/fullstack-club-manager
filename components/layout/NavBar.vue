ac5850185db506941e6f15d75e21220f32d4ce7d
Matei Nicolae - Daniel <matei1337@icloud.com>
2023-03-06 20:20:56 +0200

    if user is logged a button that redirects to the dashboard appears

--- components/layout/NavBar.vue
+++ components/layout/NavBar.vue
@@ -101,7 +101,7 @@
 						<NuxtLink
 							to="/dashboard"
 							class="inline-block rounded-lg py-1 px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900 font-display font-medium"
-							>Dashboard</NuxtLink
+							>Panou de control</NuxtLink
 						>
 					</li>
 				</ul>
@@ -178,7 +178,7 @@
 							>Legea 322</NuxtLink
 						>
 					</li>
-					<li>
+					<li v-if="!user">
 						<NuxtLink
 							to="/sign-up"
 							@click="isOpen = false"
@@ -190,6 +190,14 @@
 							@click="isOpen = false"
 							class="w-full text-center font-semibold inline-block bg-transparent border hover:bg-slate-100 px-3 py-2 rounded text-black"
 							>Logare</NuxtLink
+						>
+					</li>
+					<li v-if="user">
+						<NuxtLink
+							to="/dashboard"
+							@click="isOpen = false"
+							class="mt-8 mb-4 w-full text-center font-semibold inline-block bg-blue-500 hover:bg-blue-600 px-3 py-2 rounded text-white"
+							>Panou de control</NuxtLink
 						>
 					</li>
 				</ul>
