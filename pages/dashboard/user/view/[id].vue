d4b0f5b69dc973f0830e7723808c63fc9427ab80
Matei Nicolae - Daniel <matei1337@icloud.com>
2023-03-09 21:45:53 +0200

    UIs for all users and independent user

---
+++ pages/dashboard/user/view/[id].vue
@@ -0,0 +1,14 @@
+<template>
+	<DashboardNavBar />
+	{{ user }}
+</template>
+
+<script setup>
+const route = useRoute();
+const supabase = useSupabaseClient();
+
+const { data: user } = await supabase
+	.from("profiles")
+	.select("id, role, euroRegionMod, email, registrationDate")
+	.match({ id: route.params.id });
+</script>
